d5894069a470dc5223fdf58b68a1c01634556ff5
alanrasar <146185198+alanrasar@users.noreply.github.com>
2023-09-27 10:51:17 +0000

    Added a score system. Winner of each round is announced. Player is able to exit the progam.

--- main.py
+++ main.py
@@ -1,18 +1,43 @@
 import random
 
-computerAnswer == "NIL"
+continue_game = True
+user_score = 0
+computer_score = 0
 
-def computerChoice():
-  randomNum = random.randint(1,3)
-  if randomNum == 1:
-    computerAnswer == "rock"
-  elif randomNum == 2:
-    computerAnswer == "paper"
-  else:
-    computerAnswer == "scissors"
-  
-  
+while continue_game:
+    user_input = input("Rock, paper, or scissors? (Type 'quit' to exit): ").lower()
+    
+    if user_input == 'quit':
+        continue_game = False
+        break
+    
+    if user_input not in ['rock', 'paper', 'scissors']:
+        print("Invalid input. Please choose 'rock', 'paper', or 'scissors'.")
+        continue
 
+    random_num = random.randint(1, 3)
+    
+    if random_num == 1:
+        computer_answer = "rock"
+    elif random_num == 2:
+        computer_answer = "paper"
+    else:
+        computer_answer = "scissors"
 
+    print(f"Computer chooses: {computer_answer}")
 
-playerInput = input("Rock, paper or scissors?")
+    if user_input == computer_answer:
+        print("It's a tie!")
+    elif (
+        (user_input == 'rock' and computer_answer == 'scissors') or
+        (user_input == 'paper' and computer_answer == 'rock') or
+        (user_input == 'scissors' and computer_answer == 'paper')
+    ):
+        print("You win this round!")
+        user_score += 1
+    else:
+        print("Computer wins this round!")
+        computer_score += 1
+
+print(f"Final score - You: {user_score}, Computer: {computer_score}")
+print("Thanks for playing!")
